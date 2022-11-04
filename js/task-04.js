4bf04e933e15c02e84c0f06fd27534d861b9075d
Borys Supreme <bvs.bromes@gmail.com>
2022-11-04 03:04:53 +0200

    add json

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,15 @@
-const counterValueRef = document.querySelector("#value");
+const counterValueRef = document.querySelector('#value');
 const decrementBtnRef = document.querySelector("[data-action = 'decrement']");
 const incrementBtnRef = document.querySelector("[data-action = 'increment']");
 
 let counterValue = 0;
 
-decrementBtnRef.addEventListener("click", () => {
+decrementBtnRef.addEventListener('click', () => {
   counterValue -= 1;
   //  console.log(counterValue);
   counterValueRef.textContent = counterValue;
 });
-incrementBtnRef.addEventListener("click", () => {
+incrementBtnRef.addEventListener('click', () => {
   counterValue += 1;
   //   console.log(counterValue);
   counterValueRef.textContent = counterValue;
