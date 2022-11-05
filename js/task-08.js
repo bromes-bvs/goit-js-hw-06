b6806606b04f973dd74bab61a68579c1ae0f06ac
Borys Supreme <bvs.bromes@gmail.com>
2022-11-05 02:18:56 +0200

    finish tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginFormRef = document.querySelector('.login-form');
 
+loginFormRef.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  //   console.log(formElements);
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email === '' || password === '') {
+    alert('Все поля должны быть заполнены');
+    return;
+  }
+  const formData = { email, password };
+  console.log(formData);
+  loginFormRef.reset();
+
+  return formData;
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+changeColorBtnRef.addEventListener('click', handlechangeColor);
+
+function handlechangeColor() {
+  const bodyRef = document.body;
+
+  spanRef.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
